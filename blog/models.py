b00f185d38e8254c760ef11d078c239014842e1b
henjin93 <lubin.andre@gmail.com>
2022-03-11 20:57:06 +1100

    Blog
    Adding blog template

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
+from pyexpat import model
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+    article = models.CharField(max_length=300)
+    author = models.CharField(max_length=50)
+    date = models.DateField()
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 # Create your views here.
 
 def home(request):
-    return render(request, 'blog/home.html')
+    articles = Article.objects.all()
+    return render(request, 'blog/home.html', {'articles' : articles})
